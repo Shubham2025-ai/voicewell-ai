b7a864f957bedb4172344d47aa4155e99ea4057f
Shubham2025-ai <shubham.bhukya24@vit.edu>
2026-03-25 07:21:32 +0530

    Fixing main.jsx issue releted to bug giving double input values

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import SkeletonLoader from './components/SkeletonLoader.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <Suspense fallback={<SkeletonLoader />}>
-        <App />
-      </Suspense>
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <Suspense fallback={<SkeletonLoader />}>
+      <App />
+    </Suspense>
+  </ErrorBoundary>
 )
